18593119441804d7056cf6d295a49f8fdf716e34
Rizalul Fiqri <fiqrijambi@gmail.com>
2024-08-20 21:05:26 +0700

    breeze installation

--- database/migrations/2024_08_19_135815_create_users_table.php
+++ database/migrations/2024_08_19_135815_create_users_table.php
@@ -13,23 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('first_name', 50)->nullable();
-            $table->string('last_name', 50)->nullable();
-            $table->date('birth_of_day')->nullable();
-            $table->string('address1', 255)->nullable();
-            $table->string('address2', 255)->nullable();
-            $table->string('city', 50)->nullable();
-            $table->string('state', 50)->nullable();
-            $table->string('phone', 20)->nullable();
-            $table->string('country', 50)->nullable();
-            $table->string('avatar', 255)->nullable();
-            $table->boolean('is_active')->default(true);
-            $table->date('last_login')->nullable();
-            $table->string('activation', 50)->nullable();
-            $table->string('password', 100);
+            $table->string('name')->default('');
+            $table->string('role')->default('user');
+            $table->string('email')->unique()->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
