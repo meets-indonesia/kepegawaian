01ab33ede7ef8e627d47c05c49904d60fd305af5
kevinnaserwan <kevinnaserwan@gmail.com>
2024-08-23 22:02:34 +0700

    Feat: add superadminoradmin middleware

--- app/Http/Middleware/SuperAdminOrAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminOrAdminMiddleware.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class SuperAdminMiddleware
+class SuperAdminOrAdminMiddleware
 {
     /**
      * Handle an incoming request.
