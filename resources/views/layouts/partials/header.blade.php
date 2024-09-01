8edb1b5c1673d5e7ff805ab0bcca0244dc831258
Rizalul Fiqri <107758410+TheSwordSlay@users.noreply.github.com>
2024-09-01 20:55:11 +0700

    ....

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,7 +3,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo-unsri.png" alt="">
+        <img src="/assets/img/logo-unsri.png" alt="">
         <span class="d-none d-lg-block">Kepegawaian</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
